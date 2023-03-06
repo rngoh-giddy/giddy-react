8f4befe5029bfa435a209b7476492f923e7b27fd
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-03-06 11:44:34 -0600

    added bootstrap and react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import './App.css';
+
+import ThemeProvider from 'react-bootstrap/ThemeProvider';
+import Example from './components/article-components/FullWidthHero/Example';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <ThemeProvider
+    breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}
+    minBreakpoint="xxs"
+  >
+    <Example />
+  </ThemeProvider>
   );
 }
 
