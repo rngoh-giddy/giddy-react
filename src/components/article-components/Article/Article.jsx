0465666b1e58b0a795f841e407ba27de5ace81cd
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-03-07 17:23:57 -0600

    rendering title, author name, and image

--- src/components/article-components/Article/Article.jsx
+++ src/components/article-components/Article/Article.jsx
@@ -3,19 +3,22 @@ import { useEffect, useState } from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import ArticleHeader from './ArticleHeader';
+import ArticleAuthor from './ArticleAuthor';
+import ArticleImage from './ArticleImage';
+
 export default function Article() {
-    const [articles, setArticles] = useState([]);
-    // const value = { articles, setArticles };
+    const [article, setArticle] = useState([]);
+    const { author, title, body, image  } = article;
     const { id } = useParams();
-    console.log(id);
+    // console.log(article);
 
     useEffect(() => {
         fetch(`https://api.getmegiddyapi.com/article/drupal/${id}`)
             .then((res) => res.json())
             .then((data) => {
-                console.log(data);
-                console.log(data.article.author.name);
-                setArticles(data);
+                // console.log(data.article);
+                setArticle(data.article);
             })
             .catch((err) => {
                 console.log(err.message);
@@ -23,6 +26,11 @@ export default function Article() {
     }, []);
   
   return (
-    <div>Article Hardcode text</div>
+    <>
+      <div>Article Hardcode text</div>
+      {article?.title && <ArticleHeader title={title} />}
+      {article?.author && <ArticleAuthor author={author} />}
+      {article?.image && <ArticleImage image={image} />}
+    </>
   )
 }
---
+++ src/components/article-components/Article/ArticleAuthor.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ArticleAuthor({ author }) {
+  return (
+    <p>{author?.name}</p>
+  )
+}
