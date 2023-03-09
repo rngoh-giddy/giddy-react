87e324f9e9a8e66e3c5dfde32541bf3364b7e62e
allanGiddy <a.hillman@getmegiddy.com>
2023-03-09 16:57:19 -0600

    added-file-made-edits-to-page-render-components

--- src/components/article-components/Article/ArticleAuthor.jsx
+++ src/components/article-components/Article/ArticleAuthor.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 export default function ArticleAuthor({ author, published }) {
   return (
-    <p>Written by: {author?.name} | Published {published} </p>
+    <div>
+    <p className='fw-bold font-size-12 mb-1'>By: {author?.name} </p>
+    <p className='font-size-12'>{published} </p>
+  </div>
   )
 }
