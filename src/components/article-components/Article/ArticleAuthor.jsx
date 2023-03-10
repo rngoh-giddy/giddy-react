5bd1e68d3c4e25e064bbdac6a762302bb3ed7310
bpalmergiddy <b.palmer@getmegiddy.com>
2023-03-10 09:17:16 -0600

    Fixing some small css issues

--- src/components/article-components/Article/ArticleAuthor.jsx
+++ src/components/article-components/Article/ArticleAuthor.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export default function ArticleAuthor({ author, published }) {
   return (
     <div>
-    <p className='fw-bold font-size-12 mb-1'>By: {author?.name} </p>
-    <p className='font-size-12'>{published} </p>
+    <p>Written by: {author?.name} | Published {published} </p>
   </div>
   )
 }
