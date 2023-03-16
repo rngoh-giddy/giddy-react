c3f759dbfd17a79c60a29d4c8e2afa1aa422a695
Evan Poe <46730264+EvanPoe@users.noreply.github.com>
2023-03-16 17:30:54 -0500

    related article desktop styling

--- src/components/article-components/related-articles/RelatedArticlesContainer.jsx
+++ src/components/article-components/related-articles/RelatedArticlesContainer.jsx
@@ -39,15 +39,15 @@ export default function RelatedArticlesContainer({ id, title }) {
           className="related-article-container d-flex flex-column-reverse flex-lg-row mb-4"
           key={index}
         >
-          <img className="mx-auto mb-3" src={article?.image} alt="giddy" />
+          <img className="mx-auto mb-3 mb-lg-auto my-lg-auto" src={article?.image} alt="giddy" />
           <div className="related-article-text-container mx-auto pt-3">
-            <p className="taxonomy">
+            <p className="taxonomy mb-lg-2">
               {article?.taxonomy.associated.length
                 ? article?.taxonomy.associated[0]?.name
                 : article?.taxonomy.primary[0]?.name}
             </p>
             <h3>{article?.title}</h3>
-            <p className="article-author">By {article?.author.name}</p>
+            <p className="article-author py-0">By {article?.author.name}</p>
             <p className="article-deck">{article?.deck}</p>
           </div>
         </div>
